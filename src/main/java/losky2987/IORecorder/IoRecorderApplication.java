25728417839e5a08730a42ebc47891094607bdb5
losky2987 <losky2987@gmail.com>
2026-03-11 10:22:18 +0100

    init springboot project

---
+++ src/main/java/losky2987/IORecorder/IoRecorderApplication.java
@@ -0,0 +1,13 @@
+package losky2987.IORecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoRecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/losky2987/IORecorder/IoRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package losky2987.IORecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
